87c24aede2b03dd779aaefaaf8264a2ccff60b83
THEEKSHANA88 <thenuwan.20200911@iit.ac.lk>
2023-04-02 21:12:09 +0530

    added: new route for get quections from backend and connected.

--- chatbot_api/app.py
+++ chatbot_api/app.py
@@ -1,12 +1,35 @@
 import os
 from flask import Flask, jsonify, request
 
+import prediction
+
 app = Flask(__name__)
 app.env = "development"
 
 app.secret_key = "ChatBotApp"
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+global msg
+global questions
+
+msg = [
+        "Hi! I'm a depression screening chat bot. Let's get started.",
+        "Nice to meet you! I'm here to help you determine your depression level.",
+        "Please answer the following questions with a sentence or two:",
+    ]
+
+# Ask questions to screen for depression levels
+questions = [
+    "1. How are you feeling today?",
+    "2. Have you been experiencing any changes in your appetite?",
+    "3. Are you finding it hard to get out of bed in the morning?",
+    "4. Have you been feeling isolated or alone lately?",
+    "5. Have you lost interest in activities that you used to enjoy?",
+    "6. Have you been having any trouble sleeping?",
+    "7. Have you been feeling hopeless or like things will never get better?",
+    "8. Have you been having any thoughts of self-harm or suicide?"
+]
 
 # Route for chat
 @app.route('/api/chat', methods=['GET', 'POST'])
@@ -26,6 +49,13 @@ def chat():
         return jsonify({'status': "success", 'msg': "This is a auto generate msg!"})
 
     return jsonify({'status': "error", 'msg': "Something went wrong!"})
+
+
+# Route for initial bot msg
+@app.route('/api/initial', methods=['GET', 'POST'])
+def initial():
+
+    return jsonify({'msg': msg, "questions": questions})
 
 
 # Main
