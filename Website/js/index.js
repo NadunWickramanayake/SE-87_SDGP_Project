150e964aea0bbace53f4626a9c8042e8a4c2211f
KDA32 <119871100+KDA32@users.noreply.github.com>
2023-04-06 01:23:06 +0530

    Add files via upload

---
+++ Website/js/index.js
@@ -0,0 +1,305 @@
+
+function question01() {
+    total=0;
+    //Question 01
+    let Question01 = document.getElementsByName('Q1');
+    for (let Q1 of Question01) {
+       if (Q1.checked) {
+        if(Q1==O1){
+            total=total+0;
+            alert("The answer you have selected is - Not at all");
+        }
+        else if(Q1==O2){
+            total=total+1;
+            alert("The answer you have selected is - Several Days");
+        }
+        else if(Q1==O3){
+            total=total+2;
+            alert("The answer you have selected is - More than half the Days");
+        }
+        else if(Q1==O4){
+            total=total+3;
+            alert("The answer you have selected is - Nearly every day");
+        }
+        document.getElementById("demo").innerHTML = "Mark for question 01 = "+total;
+        Mark01=total;
+      }
+       
+    }
+
+}
+
+function question02() {
+    total=0;
+    //Question 01
+    let Question02 = document.getElementsByName('Q2');
+    for (let Q2 of Question02) {
+       if (Q2.checked) {
+        if(Q2==O21){
+            total=total+0;
+            alert("The answer you have selected is - Not at all");
+        }
+        else if(Q2==O22){
+            total=total+1;
+            alert("The answer you have selected is - Several Days");
+        }
+        else if(Q2==O23){
+            total=total+2;
+            alert("The answer you have selected is - More than half the Days");
+        }
+        else if(Q2==O24){
+            total=total+3;
+            alert("The answer you have selected is - Nearly every day");
+        }
+        document.getElementById("demo01").innerHTML = "Mark for question 02 = "+total;
+        Mark02=total;
+      }
+       
+    }
+
+}
+
+
+function question03() {
+    total=0;
+    //Question 03
+    let Question03 = document.getElementsByName('Q3');
+    for (let Q3 of Question03) {
+       if (Q3.checked) {
+        if(Q3==O31){
+            total=total+0;
+            alert("The answer you have selected is - Not at all");
+        }
+        else if(Q3==O32){
+            total=total+1;
+            alert("The answer you have selected is - Several Days");
+        }
+        else if(Q3==O33){
+            total=total+2;
+            alert("The answer you have selected is - More than half the Days");
+        }
+        else if(Q3==O34){
+            total=total+3;
+            alert("The answer you have selected is - Nearly every day");
+        }
+        document.getElementById("demo02").innerHTML = "Mark for question 03 = "+total;
+        Mark03=total;
+      }
+       
+    }
+
+}
+
+function question04() {
+    total=0;
+    //Question 04
+    let Question04 = document.getElementsByName('Q4');
+    for (let Q4 of Question04) {
+       if (Q4.checked) {
+        if(Q4==O41){
+            total=total+0;
+            alert("The answer you have selected is - Not at all");
+        }
+        else if(Q4==O42){
+            total=total+1;
+            alert("The answer you have selected is - Several Days");
+        }
+        else if(Q4==O43){
+            total=total+2;
+            alert("The answer you have selected is - More than half the Days");
+        }
+        else if(Q4==O44){
+            total=total+3;
+            alert("The answer you have selected is - Nearly every day");
+        }
+        document.getElementById("demo03").innerHTML = "Mark for question 04 = "+total;
+        Mark04=total;
+      }
+       
+    }
+
+}
+
+function question05() {
+    total=0;
+    //Question 05
+    let Question05 = document.getElementsByName('Q5');
+    for (let Q5 of Question05) {
+       if (Q5.checked) {
+        if(Q5==O51){
+            total=total+0;
+            alert("The answer you have selected is - Not at all");
+        }
+        else if(Q5==O52){
+            total=total+1;
+            alert("The answer you have selected is - Several Days");
+        }
+        else if(Q5==O53){
+            total=total+2;
+            alert("The answer you have selected is - More than half the Days");
+        }
+        else if(Q5==O54){
+            total=total+3;
+            alert("The answer you have selected is - Nearly every day");
+        }
+        document.getElementById("demo04").innerHTML = "Mark for question 05 = "+total;
+        Mark05=total;
+      }
+       
+    }
+
+}
+
+function question06() {
+    total=0;
+    //Question 06
+    let Question06 = document.getElementsByName('Q6');
+    for (let Q6 of Question06) {
+       if (Q6.checked) {
+        if(Q6==O61){
+            total=total+0;
+            alert("The answer you have selected is - Not at all");
+        }
+        else if(Q6==O62){
+            total=total+1;
+            alert("The answer you have selected is - Several Days");
+        }
+        else if(Q6==O63){
+            total=total+2;
+            alert("The answer you have selected is - More than half the Days");
+        }
+        else if(Q6==O64){
+            total=total+3;
+            alert("The answer you have selected is - Nearly every day");
+        }
+        document.getElementById("demo05").innerHTML = "Mark for question 06 = "+total;
+        Mark06=total;
+      }
+       
+    }
+
+}
+
+function question07() {
+    total=0;
+    //Question 07
+    let Question07 = document.getElementsByName('Q7');
+    for (let Q7 of Question07) {
+       if (Q7.checked) {
+        if(Q7==O71){
+            total=total+0;
+            alert("The answer you have selected is - Not at all");
+        }
+        else if(Q7==O72){
+            total=total+1;
+            alert("The answer you have selected is - Several Days");
+        }
+        else if(Q7==O73){
+            total=total+2;
+            alert("The answer you have selected is - More than half the Days");
+        }
+        else if(Q7==O74){
+            total=total+3;
+            alert("The answer you have selected is - Nearly every day");
+        }
+        document.getElementById("demo06").innerHTML = "Mark for question 07 = "+total;
+        Mark07=total;
+      }
+       
+    }
+
+}
+
+function question08() {
+    total=0;
+    //Question 08
+    let Question08 = document.getElementsByName('Q8');
+    for (let Q8 of Question08) {
+       if (Q8.checked) {
+        if(Q8==O81){
+            total=total+0;
+            alert("The answer you have selected is - Not at all");
+        }
+        else if(Q8==O82){
+            total=total+1;
+            alert("The answer you have selected is - Several Days");
+        }
+        else if(Q8==O83){
+            total=total+2;
+            alert("The answer you have selected is - More than half the Days");
+        }
+        else if(Q8==O84){
+            total=total+3;
+            alert("The answer you have selected is - Nearly every day");
+        }
+        document.getElementById("demo07").innerHTML = "Mark for question 08 = "+total;
+        Mark08=total;
+      }
+       
+    }
+
+}
+
+function question09() {
+    total=0;
+    //Question 09
+    let Question09 = document.getElementsByName('Q9');
+    for (let Q9 of Question09) {
+       if (Q9.checked) {
+        if(Q9==O91){
+            total=total+0;
+            alert("The answer you have selected is - Not at all");
+        }
+        else if(Q9==O92){
+            total=total+1;
+            alert("The answer you have selected is - Several Days");
+        }
+        else if(Q9==O93){
+            total=total+2;
+            alert("The answer you have selected is - More than half the Days");
+        }
+        else if(Q9==O94){
+            total=total+3;
+            alert("The answer you have selected is - Nearly every day");
+        }
+        document.getElementById("demo08").innerHTML = "Mark for question 09 = "+total;
+        Mark09=total;
+      }
+       
+    }
+
+}
+
+function marktotal() {
+    Maintotal=Mark01+Mark02+Mark03+Mark04+Mark05+Mark06+Mark07+Mark08+Mark09;
+    document.getElementById("demo09").innerHTML = "Total Score = "+Maintotal;
+    //Checking for depression stages
+    if(Maintotal<=4 && Maintotal>=1){
+        document.getElementById("diagnosis").innerHTML = "Minimal Depression";
+        document.getElementById("content").innerHTML ="Minimal depression, also known as minor depression or subsyndromal depression, is a type of depression that involves mild or moderate symptoms that do not meet the full criteria for major depression. Although it is often less severe than major depression, it can still have a significant impact on a person's life and well-being. The symptoms of minimal depression can include:  Feeling sad or down,  Loss of interest in activities that were previously enjoyed, Difficulty sleeping or oversleeping,  Changes in appetite or weight,  Fatigue or low energy, Difficulty concentrating or making decisions, Feelings of worthlessness or guilt, Irritability or restlessness, Physical symptoms such as headaches or stomach problems";
+
+    }
+    else if(Maintotal<=9 && Maintotal>=5){
+        document.getElementById("diagnosis").innerHTML = "Mild Depression";
+        document.getElementById("content").innerHTML = "Mild depression involves depressive symptoms that are considered low-grade. While many of the same symptoms of more severe depression are present, including irritability, sadness, and lack of motivation, they are often more subtle and less intense. People with mild, low-grade depression may not even realize they are depressed. In fact, the chronic feelings of sadness and low mood they experience may have been around for so long that they feel normal. However, it is not normal to go through life feeling unhappy all of the time. Everyone experiences occasional bouts of low mood in response to sad or stressful life events, but constantly feeling depressed does not have to be the story of your life.";
+
+
+    }
+    else if(Maintotal<=14 && Maintotal>=10){
+        document.getElementById("diagnosis").innerHTML = "Moderate Depression";
+        document.getElementById("content").innerHTML ="What does moderate depression feel like? In general, the symptoms of moderate depression are the same as the symptoms experienced in mild depression. The key difference is that the symptoms of moderate depression tend to be more intense and may be experienced more frequently. This causes greater dysfunction in daily life, including at home and at work.";
+
+    }
+    else if(Maintotal<=19 && Maintotal>=15){
+        document.getElementById("diagnosis").innerHTML = "Moderately Severe Depression";
+        document.getElementById("content").innerHTML = "Moderately severe depression is marked by two main symptoms: persistent low mood and decreased interest in activities. Some of the other symptoms of depression include: Avoiding social activities,Changes in appetite, Despair and guilt, Difficulty concentrating, Difficulty sleeping, Excessive worry, Fatigue or lack of energy, Feelings of hopelessness, Irritability, Lack of motivation, Low self-esteem. Women are almost twice as likely to experience depression as men. While moderate depression may be less severe, it does cause impairments in work, school, home, or social difficulties. Research also suggests that moderately severe depression increases certain health risks. One study found that people who were moderately depressed had a higher risk of cardiovascular disease compared to those who were severely depressed.";
+
+    }
+    else if(Maintotal<=27 && Maintotal>=20){
+        document.getElementById("diagnosis").innerHTML = "Severe Depression";
+        document.getElementById("content").innerHTML ="Severe depression is not a distinct diagnosis from major depressive disorder. Rather, people tend to experience their depression as more severe when they have several symptoms of depression or when some depression symptoms are especially intense. Depression exists on a continuum, from symptoms that mildly disrupt daily life to those that are totally debilitating. Symptoms of severe depression Insomnia, Irritability, Loss of interest in activities that used to be enjoyable, Hopelessness, Persistent thoughts of something bad happening, Thoughts of death or suicide or suicide attempts, In very severe cases, psychotic symptoms, Inability to take care of oneself, such as eating, bathing, or fulfilling family or work responsibilities. Although you might feel that there's no hope, talk to your doctor about treatment options. Even severe depression symptoms can be treated.";
+
+    }
+    
+
+}
